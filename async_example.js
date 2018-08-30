72314d1736efd800712e4058408fae9c9141078b
parmanand sahu <parmanand.sahu@gohuddl.com>
2018-08-30 15:09:58 +0530

    add promise and async example

---
+++ async_example.js
@@ -0,0 +1,24 @@
+
+/** 
+ * source:https://codeburst.io/javascript-es-2017-learn-async-await-by-example-48acc58bad65
+ */
+
+function doubleAfter2Seconds(x) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(x * 2);
+    }, 2000);
+  });
+}
+
+async function addAsync(x) {
+  const a = await doubleAfter2Seconds(10);
+  const b = await doubleAfter2Seconds(20);
+  const c = await doubleAfter2Seconds(30);
+  return x + a + b + c;
+}
+
+
+addAsync(10).then((sum) => {
+  console.log(sum);
+});
